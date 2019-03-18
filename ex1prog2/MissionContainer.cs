cb253c6351a6e2027118efe8383b31c93d388644
Osnat Ackerman <osnatackerman0376@gmail.com>
2019-03-18 16:12:13 +0200

    changes

---
+++ ex1prog2/MissionContainer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    public delegate double Func(double val);
+
+    public class FunctionsContainer
+    {
+        Dictionary<string, Func> dict = new Dictionary<string, Func>();
+        public Func this[string funcName]
+        {
+            get
+            {
+                if (!dict.ContainsKey(funcName))
+                {
+                    dict[funcName] = x => x;
+                }
+                return dict[funcName];
+            }
+
+            set
+            {
+                dict[funcName] = value;
+            }
+        }
+    }
+}
---
+++ ex1prog2/SingleMission.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    public class SingleMission : IMission
+    {
+        private Func funcToCalc;
+        private string name;
+        private string type = "Single";
+        public event EventHandler<double> OnCalculate;
+
+
+        SingleMission(Func func, string missionName)
+        {
+            funcToCalc = func;
+            name = missionName;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+        }
+
+        public double Calculate(double value)
+        {
+            OnCalculate?.Invoke(this, value);
+            return funcToCalc(value);
+        }
+
+
+    }
+}
